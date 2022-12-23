29916af4b1f38bd60514af3008e83b0df4024630
Mariusz Jacek <marios.yaceg@gmail.com>
2022-12-23 11:53:45 +0100

    Backend

---
+++ Express-TypeScript/src/app.ts
@@ -0,0 +1,27 @@
+import express, {Request, Response} from 'express'
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", (req: Request, res: Response) => {
+    return res.send(`Hello world`)
+});
+
+app.post('/api/data', (req: Request, res: Response) => {
+    console.log(req.body);
+    return res.sendStatus(200);
+    
+})
+
+app.all('/api/all', (req: Request, res: Response) => {
+    return res.sendStatus(200)
+})
+
+app.listen(8888, () => {
+    console.log(`🫐  It's working`);
+    
+})
