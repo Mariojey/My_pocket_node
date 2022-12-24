6fbeece1e1f79e829d97ad8f6450911467d5e2f9
Mariusz Jacek <marios.yaceg@gmail.com>
2022-12-24 15:22:33 +0100

    NextFunction

--- Express-TypeScript/README.md
+++ Express-TypeScript/README.md
@@ -1,2 +1,4 @@
 https://www.youtube.com/watch?v=KgnJNJk9-to
 18:40
+
+//All dependencies using in backend should be installed as devDependenxies with -D
--- Express-TypeScript/src/app.ts
+++ Express-TypeScript/src/app.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from 'express'
+import express, {NextFunction, Request, Response} from 'express'
 
 
 const app = express();
@@ -7,8 +7,16 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: true}))
 
+function handleGetBook(req: Request, res: Response, next: NextFunction){
+    console.log(req.params); 
+}
+
 app.get("/", (req: Request, res: Response) => {
     res.send({"status": true})
+})
+
+app.get("/ab*cd", (req: Request, res: Response) => {
+    res.send("/ab*cd");
 })
 
 app.post('/api/data', (req: Request, res: Response) => {
@@ -21,6 +29,12 @@ app.all('/api/all', (req: Request, res: Response) => {
     return res.sendStatus(200)
 })
 
+app.get('/api/books/:bookId', (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.params.bookId);
+    res.send('I have params')
+    
+})
+app.get('/api/cars/:carId/:carOwner', handleGetBook)
 app.listen(8888, () => {
     console.log(`🫐  It's working`);
     
